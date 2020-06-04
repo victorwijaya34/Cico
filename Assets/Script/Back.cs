025810f04a21215a870496c4d11b8be0ae62f202
victorwijaya34 <victorwijaya62@gmail.com>
2020-06-05 00:09:38 +0700

    first push

---
+++ Assets/Script/Back.cs
@@ -0,0 +1,16 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour
+{
+    public void BackPage()
+	{
+		SceneManager.LoadScene ("Pria");
+	}
+	public void BackPage2()
+	{
+		SceneManager.LoadScene ("Wanita");
+	}
+}
---
+++ Assets/Script/GendertoLayar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GendertoLayar : MonoBehaviour
+{
+    public void Layar()
+	{
+		SceneManager.LoadScene ("Layar");
+	}
+public void Layar2()
+	{
+		SceneManager.LoadScene ("Layar2");
+	}
+    public void Back()
+	{
+		SceneManager.LoadScene ("PilihGender");
+	}
+}
