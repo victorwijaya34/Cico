cbb06a129c54319a9fd50388afa2a418d24c9530
dan <thedanimron@gmail.com>
2020-06-11 02:02:18 +0700

    switch scene and add object

---
+++ Assets/Script/change.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour
+{
+    public void vans1()
+    {
+        SceneManager.LoadScene("Layar1A");
+    }
+    public void vans2()
+    {
+        SceneManager.LoadScene("Layar1B");
+    }
+    public void vansback()
+    {
+        SceneManager.LoadScene("Layar");
+    }
+
+    public void heels1()
+    {
+        SceneManager.LoadScene("Layar2A");
+    }
+    public void heels2()
+    {
+        SceneManager.LoadScene("Layar2B");
+    }
+    public void heelsback()
+    {
+        SceneManager.LoadScene("Layar2");
+    }
+}
